ccd0c6c2b0af126f9b35981b17fc1a403f0ec33a
EsinaVale <esina.v@ccam.cmc.unn.net>
2016-10-25 09:53:11 +0300

    Тесты к матрицам и векторам

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -2,110 +2,156 @@
 
 #include <gtest.h>
 
-TEST(TMatrix, can_create_matrix_with_positive_length)
+TEST(TMatrix, can_create_matrix_with_positive_length) // может создать матрицу с положительной длины
 {
   ASSERT_NO_THROW(TMatrix<int> m(5));
 }
 
-TEST(TMatrix, cant_create_too_large_matrix)
+TEST(TMatrix, cant_create_too_large_matrix) // не может создать слишком большую матрицу
 {
   ASSERT_ANY_THROW(TMatrix<int> m(MAX_MATRIX_SIZE + 1));
 }
 
-TEST(TMatrix, throws_when_create_matrix_with_negative_length)
+TEST(TMatrix, throws_when_create_matrix_with_negative_length) // не можем созадть матрицу с отрицательной длинной
 {
   ASSERT_ANY_THROW(TMatrix<int> m(-5));
 }
 
-TEST(TMatrix, can_create_copied_matrix)
+TEST(TMatrix, can_create_copied_matrix) // созадть копию матрицы
 {
   TMatrix<int> m(5);
 
   ASSERT_NO_THROW(TMatrix<int> m1(m));
 }
 
-TEST(TMatrix, copied_matrix_is_equal_to_source_one)
+TEST(TMatrix, copied_matrix_is_equal_to_source_one) // скопированая матрица равна одному источнику
 {
-  ADD_FAILURE();
+ 
+	TMatrix <int> m (5); 
+	m[0][0] = 1; 
+	TMatrix <int> m1 (m); 
+	EXPECT_EQ (m, m1); 
+
 }
 
-TEST(TMatrix, copied_matrix_has_its_own_memory)
+TEST(TMatrix, copied_matrix_has_its_own_memory) // скопирована матрица имеет свою собственную память
 {
-  ADD_FAILURE();
+	TMatrix <int> m (4);
+	TMatrix <int> m1 (m);
+    m [0][0] = 1; 
+    EXPECT_NE (m [0][0], m1 [0][0]); 
 }
 
-TEST(TMatrix, can_get_size)
+TEST(TMatrix, can_get_size) // получить размер
 {
-  ADD_FAILURE();
+	TMatrix <int> m (3); 
+	EXPECT_EQ (3, m.GetSize ()); 
 }
 
-TEST(TMatrix, can_set_and_get_element)
+TEST(TMatrix, can_set_and_get_element) // можно установить и получить элемент
 {
-  ADD_FAILURE();
+  
+	TMatrix <int> m (6); 
+	m [0][1] = 1; 
+	EXPECT_EQ (1, m [0][1]); 
 }
 
-TEST(TMatrix, throws_when_set_element_with_negative_index)
+TEST(TMatrix, throws_when_set_element_with_negative_index)  // исключение, когда установлен элемент с отрицательным индексом
 {
-  ADD_FAILURE();
+  TMatrix <int> m (3); 
+	ASSERT_ANY_THROW (m [0][-1]); 
 }
 
-TEST(TMatrix, throws_when_set_element_with_too_large_index)
+TEST(TMatrix, throws_when_set_element_with_too_large_index) // исключение, когда установлен элемент со слишком большим индексом
 {
-  ADD_FAILURE();
+  TMatrix <int> m (5); 
+    ASSERT_ANY_THROW (m [0][5]); 
 }
 
-TEST(TMatrix, can_assign_matrix_to_itself)
+TEST(TMatrix, can_assign_matrix_to_itself) // может присвоить себе матрицу
 {
-  ADD_FAILURE();
+  TMatrix <int> m (5); 
+	ASSERT_NO_THROW (m = m); 
 }
 
-TEST(TMatrix, can_assign_matrices_of_equal_size)
+TEST(TMatrix, can_assign_matrices_of_equal_size) // может назначить матрицы одинакового размера
 {
-  ADD_FAILURE();
+	TMatrix <int> m1 (3), m2 (3); 
+	for (int i = 0; i < 3; i ++) 
+		m1 [i][i] = i; 
+	m2 = m1; 
+	EXPECT_EQ (m1, m2);
 }
 
-TEST(TMatrix, assign_operator_change_matrix_size)
+TEST(TMatrix, assign_operator_change_matrix_size) // назначить оператора размер изменение матрицы
 {
-  ADD_FAILURE();
+  TMatrix <int> m1 (10), m2 (5); 
+    m1 = m2; 
+    EXPECT_EQ (5, m1.GetSize ()); 
 }
 
-TEST(TMatrix, can_assign_matrices_of_different_size)
+TEST(TMatrix, can_assign_matrices_of_different_size) // может назначить матриц различного размера
 {
-  ADD_FAILURE();
+  TMatrix <int> m1 (10), m2 (5); 
+	for (int i = 0; i < 10; i ++) 
+		m1 [i][i] = i; 
+	m2 = m1; 
+	EXPECT_EQ (m1, m2); 
 }
 
-TEST(TMatrix, compare_equal_matrices_return_true)
+TEST(TMatrix, compare_equal_matrices_return_true) // сравнить равные матрицы возвращает истину
 {
-  ADD_FAILURE();
+ TMatrix <int> m1 (3), m2 (3); 
+	EXPECT_EQ(true, m1 == m2); 
 }
 
-TEST(TMatrix, compare_matrix_with_itself_return_true)
+TEST(TMatrix, compare_matrix_with_itself_return_true) // сравнить матрицу с собой возвращает истину
 {
-  ADD_FAILURE();
+  TMatrix <int> m1 (10); 
+	EXPECT_EQ (true, m1 == m1);
 }
 
-TEST(TMatrix, matrices_with_different_size_are_not_equal)
+TEST(TMatrix, matrices_with_different_size_are_not_equal) //матрицы с различным размером не равны
 {
-  ADD_FAILURE();
+  TMatrix <int> m1 (2), m2 (4); 
+	EXPECT_EQ (true, m1 != m2); 
 }
 
-TEST(TMatrix, can_add_matrices_with_equal_size)
+TEST(TMatrix, can_add_matrices_with_equal_size) // могут добавлять матрицы с одинаковым размером
 {
-  ADD_FAILURE();
+  TMatrix <int> m1 (5), m2 (5), m (5), testM (5); 
+	m1 [0][0] = 1;
+	m1 [1][1] = 3;
+	m2 [0][0] = 5;
+	m2 [1][1] = 7;
+	m = m1 + m2; 
+	testM [0][0] = 6; 
+	testM [1][1] = 10; 
+	EXPECT_EQ (testM, m); 
 }
 
-TEST(TMatrix, cant_add_matrices_with_not_equal_size)
+TEST(TMatrix, cant_add_matrices_with_not_equal_size) // не могут добавлять матрицы с не одинакового размера
 {
-  ADD_FAILURE();
+  TMatrix <int> m1 (2), m2 (4); 
+	ASSERT_ANY_THROW (m1 + m2); 
 }
 
-TEST(TMatrix, can_subtract_matrices_with_equal_size)
+TEST(TMatrix, can_subtract_matrices_with_equal_size) // можно вычесть матрицы с одинакового размера
 {
-  ADD_FAILURE();
+  TMatrix <int> m1 (5), m2 (5), m (5), testM (5); 
+	m1 [0][0] = 7;
+	m1 [1][1] = 5; 
+	m2 [0][0] = 3;
+	m2 [1][1] = 1;
+	m = m1 - m2; 
+	testM [0][0] = 4; 
+	testM [1][1] = 4; 
+	EXPECT_EQ (testM, m); 
+
 }
 
-TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
+TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size) // не может вычесть матрицы с не одинакового размера
 {
-  ADD_FAILURE();
+  	TMatrix <int> m1 (4), m2 (2); 
+	ASSERT_ANY_THROW (m1 - m2); 
 }
-
--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -2,58 +2,68 @@
 
 #include <gtest.h>
 
-TEST(TVector, can_create_vector_with_positive_length)
+TEST(TVector, can_create_vector_with_positive_length) // может создать вектор с положительной длины
 {
   ASSERT_NO_THROW(TVector<int> v(5));
 }
 
-TEST(TVector, cant_create_too_large_vector)
+TEST(TVector, cant_create_too_large_vector) //  не может создать слишком большой вектор
 {
   ASSERT_ANY_THROW(TVector<int> v(MAX_VECTOR_SIZE + 1));
 }
 
-TEST(TVector, throws_when_create_vector_with_negative_length)
+TEST(TVector, throws_when_create_vector_with_negative_length) // исключение при создании вектора с отрицательной длиной
 {
   ASSERT_ANY_THROW(TVector<int> v(-5));
 }
 
-TEST(TVector, throws_when_create_vector_with_negative_startindex)
+TEST(TVector, throws_when_create_vector_with_negative_startindex) // исключение при создании вектора с отрицательной StartIndex
 {
   ASSERT_ANY_THROW(TVector<int> v(5, -2));
 }
 
-TEST(TVector, can_create_copied_vector)
+TEST(TVector, can_create_copied_vector) // может создать копию вектора
 {
   TVector<int> v(10);
 
   ASSERT_NO_THROW(TVector<int> v1(v));
 }
 
-TEST(TVector, copied_vector_is_equal_to_source_one)
+TEST(TVector, copied_vector_is_equal_to_source_one) // копия вектора равна  исходному вектору
 {
-  ADD_FAILURE();
+	TVector<int> v(10);
+	TVector<int> v1(v);
+
+	EXPECT_EQ(1, v == v1);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, copied_vector_has_its_own_memory)
+TEST(TVector, copied_vector_has_its_own_memory) // скопированный вектор имеет свою собственную память
 {
-  ADD_FAILURE();
+	TVector<int> v(10);
+	TVector<int> v1(v);
+	v[0] = 321;		// у одного вектора нулевую ячейку зададим 321 у другого 123, если бы у них была одна память то и у v и v1 указывали бы на последнее присовееное значение, то есть 123
+	v1[0] = 123;	// а если у них разная память, то каждый будет указывать на память со своим значением
+
+	EXPECT_EQ(0, v[0] == v1[0]);
+  //ADD_FAILURE();
 }
 
-TEST(TVector, can_get_size)
+TEST(TVector, can_get_size) // проверить размер
 {
   TVector<int> v(4);
 
   EXPECT_EQ(4, v.GetSize());
 }
 
-TEST(TVector, can_get_start_index)
+TEST(TVector, can_get_start_index) // проверить первый индекс вектора
 {
   TVector<int> v(4, 2);
 
   EXPECT_EQ(2, v.GetStartIndex());
 }
 
-TEST(TVector, can_set_and_get_element)
+TEST(TVector, can_set_and_get_element) // проверить присваивание значения элементу вектора
 {
   TVector<int> v(4);
   v[0] = 4;
@@ -61,93 +71,218 @@ TEST(TVector, can_set_and_get_element)
   EXPECT_EQ(4, v[0]);
 }
 
-TEST(TVector, throws_when_set_element_with_negative_index)
+TEST(TVector, throws_when_set_element_with_negative_index) // исключение, когда установлен элемент с отрицательным индексом
 {
-  ADD_FAILURE();
+	TVector<int> v(4);
+
+	ASSERT_ANY_THROW(v[-3]);
+  //ADD_FAILURE();
 }
 
-TEST(TVector, throws_when_set_element_with_too_large_index)
+TEST(TVector, throws_when_set_element_with_too_large_index) // исключение, когда установлен элемент со слишком большим индексом
 {
-  ADD_FAILURE();
+	TVector<int> v(4,2);
+
+	ASSERT_ANY_THROW(v[8]);
+  //ADD_FAILURE();
 }
 
-TEST(TVector, can_assign_vector_to_itself)
+TEST(TVector, can_assign_vector_to_itself) // может присвоить себе вектор  
 {
-  ADD_FAILURE();
+	TVector <int> v1 (4);
+	ASSERT_NO_THROW (v1 = v1); 
 }
 
-TEST(TVector, can_assign_vectors_of_equal_size)
+TEST(TVector, can_assign_vectors_of_equal_size) // может присваивать векторов одинакового размера 
 {
-  ADD_FAILURE();
+	const int size1 = 2, size2 = 5;
+	TVector<int> v1(size1), v2(size2);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+	}
+	v2 = v1;
+
+	EXPECT_EQ(v2, v1);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, assign_operator_change_vector_size)
+TEST(TVector, assign_operator_change_vector_size) // присваивать оператор изменения вектора размера 
 {
-  ADD_FAILURE();
+	const int size1 = 2, size2 = 5;
+	TVector<int> v1(size1), v2(size2);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+	}
+	v2 = v1;
+
+	EXPECT_EQ(v2, v1);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, can_assign_vectors_of_different_size)
+TEST(TVector, can_assign_vectors_of_different_size) // может назначить векторы разного размера 
 {
-  ADD_FAILURE();
+		TVector <int> v1 (3), v2 (5);
+	for (int i = 0; i < 3; i ++)
+		v1 [i] = 1;
+	v2 = v1;
+	EXPECT_NE (0, v2 [0]);
+	
 }
 
-TEST(TVector, compare_equal_vectors_return_true)
+TEST(TVector, compare_equal_vectors_return_true) // сравнить равные векторы возвращает истину
 {
-  ADD_FAILURE();
+	const int size1 = 2, size2 = 5;
+	TVector<int> v1(size1), v2(size2);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+	}
+	v2 = v1;
+
+	EXPECT_EQ(1,v2 == v1);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, compare_vector_with_itself_return_true)
+TEST(TVector, compare_vector_with_itself_return_true) // сравнить вектор с собой возвращает истину 
 {
-  ADD_FAILURE();
+	TVector<int> v1(5);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+	}
+	TVector<int> v2(v1);
+
+	EXPECT_EQ(1,v2 == v1);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, vectors_with_different_size_are_not_equal)
+TEST(TVector, vectors_with_different_size_are_not_equal) // векторы с различным размером не равны
 {
-  ADD_FAILURE();
+	TVector<int> v1(5), v2(10);
+
+	EXPECT_EQ(1,v2 != v1);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, can_add_scalar_to_vector)
+TEST(TVector, can_add_scalar_to_vector) // можно добавить скаляр вектора
 {
-  ADD_FAILURE();
+	TVector<int> v1(5);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+	}
+	v1[0] = v1[0] + 3;
+
+	EXPECT_EQ(3,v1[0]);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, can_subtract_scalar_from_vector)
+TEST(TVector, can_subtract_scalar_from_vector) // можно вычесть скаляр из вектора
 {
-  ADD_FAILURE();
+	TVector<int> v1(5);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+	}
+	v1[4] = v1[4] - 2;
+
+	EXPECT_EQ(2,v1[4]);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, can_multiply_scalar_by_vector)
+TEST(TVector, can_multiply_scalar_by_vector) // можно умножить скалярные вектором
 {
-  ADD_FAILURE();
+	TVector<int> v1(5);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+	}
+	v1[4] = v1[4]*2;
+
+	EXPECT_EQ(8,v1[4]);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, can_add_vectors_with_equal_size)
+TEST(TVector, can_add_vectors_with_equal_size) // могут добавлять векторы с одинаковым размером
 {
-  ADD_FAILURE();
+	TVector<int> v1(5), v2(5), v3(5);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+		v2[i] = i;
+		v3[i] = i*2;
+	}	
+
+	EXPECT_EQ(v3, v1 + v2);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, cant_add_vectors_with_not_equal_size)
+TEST(TVector, cant_add_vectors_with_not_equal_size) // не могут добавлять векторы с не одинакового размера
 {
-  ADD_FAILURE();
+	TVector<int> v1(5), v2(10);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+		v2[i] = i;
+	}	
+
+	EXPECT_EQ(NULL, v1 + v2);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, can_subtract_vectors_with_equal_size)
+TEST(TVector, can_subtract_vectors_with_equal_size) // можно вычесть векторы с одинакового размера
 {
-  ADD_FAILURE();
+	TVector<int> v1(5), v2(5), v3(5);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+		v2[i] = i;
+		v3[i] = 0;
+	}	
+
+	EXPECT_EQ(v3, v1 - v2);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, cant_subtract_vectors_with_not_equal_size)
+TEST(TVector, cant_subtract_vectors_with_not_equal_size) // не могут вычитать векторы с не одинакового размера
 {
-  ADD_FAILURE();
+	TVector<int> v1(5), v2(10);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+		v2[i] = i;
+	}	
+
+	EXPECT_EQ(NULL, v1 - v2);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, can_multiply_vectors_with_equal_size)
+TEST(TVector, can_multiply_vectors_with_equal_size) // можно умножить векторы с одинакового размера
 {
-  ADD_FAILURE();
+	TVector<int> v1(5), v2(5), v3(5);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = 2;
+		v2[i] = 3;
+		v3[i] = 6;
+	}	
+
+	EXPECT_EQ(v3, v1 * v2);
+	//ADD_FAILURE();
 }
 
-TEST(TVector, cant_multiply_vectors_with_not_equal_size)
+TEST(TVector, cant_multiply_vectors_with_not_equal_size) // нельзя умножить векторы с одинакового размера
 {
-  ADD_FAILURE();
+	TVector<int> v1(5), v2(10);
+	for (int i = 0; i < v1.GetSize(); i++)
+	{
+		v1[i] = i;
+		v2[i] = i;
+	}	
+
+	EXPECT_EQ(NULL, v1 * v2);
+	//ADD_FAILURE();
 }
 
